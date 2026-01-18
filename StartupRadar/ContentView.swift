b1da3b5d94d40304a50b8ff3f7efca47ccfe686d
Jack <qianquandong@gmail.com>
2026-01-18 14:44:46 -0600

    Initial Commit

---
+++ StartupRadar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StartupRadar
+//
+//  Created by Cheryl's on 2026/1/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StartupRadar/StartupRadarApp.swift
@@ -0,0 +1,17 @@
+//
+//  StartupRadarApp.swift
+//  StartupRadar
+//
+//  Created by Cheryl's on 2026/1/18.
+//
+
+import SwiftUI
+
+@main
+struct StartupRadarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
